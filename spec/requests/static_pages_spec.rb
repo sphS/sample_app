67d8fe9eef22d3d174dbca27ae743176946eb33b
sphS <ruman_kz@mail.ru>
2017-07-01 10:39:58 +0600

    Finish layouts and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,28 +3,27 @@ require 'rails_helper'
 
 describe "Static pages" do
 
+  subject {page}
   describe "Home page" do
+    before {visit root_path}
 
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+    it {should have_content('Sample App')}
+    it {should have_title(full_title(''))}
+    it {should_not have_title(full_title('Home'))}
+
     end
 
-    it "should have the right title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-    end
-  end
+
 
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the right title" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
@@ -32,13 +31,26 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
 
     it "should have the right title" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contacts'" do
+      visit contact_path
+      expect(page).to have_content('Contacts')
+    end
+
+    it "should have the right title" do
+      visit contact_path
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contacts")
     end
   end
 end
